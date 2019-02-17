3b1d3d2cdb8e1430b5c5599f61f51d0ad0bbd03e
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-02-17 15:36:36 -0500

    Add code for creating element and text that will go in element

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // Define variables
 var button = document.getElementsByTagName('button')[0];
 var lunchItemWrapper = document.getElementById('generator-results');
+
+var ourRandomLunchItem = document.createElement('p');
+lunchItemWrapper.appendChild(ourRandomLunchItem);
+
+var placeholdertext = document.createTextNode('');
+ourRandomLunchItem.appendChild(placeholdertext);
