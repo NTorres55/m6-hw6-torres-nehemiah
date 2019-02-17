6fc70399243764e4b15a3c5cde1049466c3dfcba
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-02-17 16:47:19 -0500

    Add apostrophe's to string and placeholder space for text

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ var ourRandomLunchItem = document.createElement('p');
 var lunchItemWrapper = document.getElementById('generator-results');
 lunchItemWrapper.appendChild(ourRandomLunchItem);
 
-var lunchItems = ['Bellas Wood Fired Pizza & Tapas', 'Carols Kitchen', 'Cousins Maine Lobster',
-'Cow Bar', 'Curry in a Hurry', 'Iylas Southern Kitchen', 'Makus Empanadas', 'MKG Kitchen', 'Oak City Fish & Chips', 'Sassool', 'The Bowls', 'The Broth', 'Wicked Taco', 'YoHo Hibachi & Sushi Burrito'];
+var placeholderText = document.createElement('p');
+
+var lunchItems = ['Bella\'s Wood Fired Pizza & Tapas', 'Carroll\'s Kitchen', 'Cousins Maine Lobster',
+'Cow Bar', 'Curry in a Hurry', 'Iyla\'s Southern Kitchen', 'Makus Empanadas', 'MKG Kitchen', 'Oak City Fish & Chips', 'Sassool', 'The Bowls', 'The Broth', 'Wicked Taco', 'YoHo Hibachi & Sushi Burrito'];
 
 var myFunction = function() {
   lunchItems.sort(function(a, b){return 0.5 - Math.random()});
