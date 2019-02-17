ec4e4251b4ae372b6fbe1765a119e130b8c4a37a
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-02-17 16:31:35 -0500

    Add list of lunch items

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 var button = document.getElementsByTagName('button')[0];
 var myHeading = document.getElementsByTagName('h1')[0];
 
+var lunchItems = ['Bellas Wood Fired Pizza & Tapas', 'Carols Kitchen', 'Cousins Maine Lobster',
+'Cow Bar', 'Curry in a Hurry', 'Iylas Southern Kitchen', 'Makus Empanadas', 'MKG Kitchen', 'Oak City Fish & Chips', 'Sassool', 'The Bowls', 'The Broth', 'Wicked Taco', 'YoHo Hibachi & Sushi Burrito'];
+
 var myFunction = function() {
   myHeading.classList.add('big-and-bold');
 };
