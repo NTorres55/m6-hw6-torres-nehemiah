578ba4a0f0792097c675b4e06fe794174e69c7cf
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-02-17 16:18:09 -0500

    Add function

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 // Define variables
 var button = document.getElementsByTagName('button')[0];
-var lunchItemWrapper = document.getElementById('generator-results');
+var myHeading = document.getElementsByTagName('h1')[0];
+
+var myFunction = function() {
+  myHeading.classList.add('big-and-bold');
+};
+
+button.onclick = myFunction;
+
+
+//var lunchItemWrapper = document.getElementById('generator-results');
 
 var ourRandomLunchItem = document.createElement('p');
 lunchItemWrapper.appendChild(ourRandomLunchItem);
@@ -12,9 +21,7 @@ var lunchItems = ['Bellas Wood Fired Pizza & Tapas', 'Carols Kitchen', 'Cousins 
 'Cow Bar', 'Curry in a Hurry', 'Iylas Southern Kitchen', 'Makus Empanadas', 'MKG Kitchen', 'Oak City Fish & Chips', 'Sassool', 'The Bowls', 'The Broth', 'Wicked Taco', 'YoHo Hibachi & Sushi Burrito'];
 
 
-var myFunction = function() {
-
-  lunchItems.sort(function(a, b){return 0.5 - Math.random();});
+  lunchItems.sort(function(a, b){return 0.5 - Math.random()});
 
   var ourRandomLunchItemFiller = document.createTextNode(lunchItems[0]);
   ourRandomLunchItem.appendChild(ourRandomLunchItemFiller);
