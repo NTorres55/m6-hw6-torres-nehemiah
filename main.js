5205c7ce8e22c42b9dd895dc34168fdad2bbf0c7
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-02-17 16:55:36 -0500

    Add random filler to function

--- main.js
+++ main.js
@@ -6,14 +6,16 @@ var ourRandomLunchItem = document.createElement('p');
 var lunchItemWrapper = document.getElementById('generator-results');
 lunchItemWrapper.appendChild(ourRandomLunchItem);
 
-var placeholderText = document.createElement('p');
+var placeholderText = document.createTextNode('');
+ourRandomLunchItem.appendChild(placeholderText);
 
 var lunchItems = ['Bella\'s Wood Fired Pizza & Tapas', 'Carroll\'s Kitchen', 'Cousins Maine Lobster',
 'Cow Bar', 'Curry in a Hurry', 'Iyla\'s Southern Kitchen', 'Makus Empanadas', 'MKG Kitchen', 'Oak City Fish & Chips', 'Sassool', 'The Bowls', 'The Broth', 'Wicked Taco', 'YoHo Hibachi & Sushi Burrito'];
 
 var myFunction = function() {
-  lunchItems.sort(function(a, b){return 0.5 - Math.random()});
-  console.log(lunchItems);
+  lunchItems.sort(function(a, b){return 0.5 - Math.random();});
+  var ourRandomLunchItemFiller = document.createTextNode('lunchItems[0]');
+  ourRandomLunchItem.appendChild(ourRandomLunchItemFiller);
 };
 
 button.onclick = myFunction;
